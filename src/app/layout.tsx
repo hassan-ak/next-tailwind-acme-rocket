9c8ae1bc42fb6c2f736ecf3de015de599416fdf4
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-02-17 01:24:22 +0500

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
-import './globals.css';
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import "./globals.css";
 
 export default function RootLayout({
   children,
@@ -6,9 +8,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
+        <Header />
+        <main className="widescreen:section-min-height tallscreen:section-min-height mx-auto max-w-4xl">
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html>
   );
 }
